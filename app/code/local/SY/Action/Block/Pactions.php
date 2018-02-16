87c06a0cc29ac1ae55aba1e2b3c9ced664f48ca8
yeloff84 <serhiy.yelahin@gmail.com>
2018-02-16 09:39:55 +0200

    ref#35131 Створити модуль акцій (expired product actions visibility fix)

--- app/code/local/SY/Action/Block/Pactions.php
+++ app/code/local/SY/Action/Block/Pactions.php
@@ -10,11 +10,20 @@ class Sy_Action_Block_Pactions extends Mage_Core_Block_Template
     {
         $productId = $this->getRequest()->getParam('id');
 
+        /** @var Mage_Core_Model_Date $datemodel */
+        $datemodel = Mage::getModel('core/date');
+        $curdate = $datemodel->gmtDate('Y-m-d H:i:s');
+
         $model = Mage::getModel('action/action');
-        $pmodel = Mage::getModel('action/products');
+
         $actions = $model->getCollection()
             ->join(['a'=>'action/products'], 'main_table.id=a.action_id', null, 'left')
-            ->addFieldToFilter('a.product_id', $productId);
+            ->addFieldToFilter('a.product_id', $productId)
+            ->addFieldToFilter('start_datetime', array('lteq' => $curdate))
+            ->addFieldToFilter('end_datetime', array(
+                array('gteq' => $curdate),
+                array('null' => true)
+            ));
 
         return $actions;
     }
